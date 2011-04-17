dc73ebfa53d0e74aa9df30453a9da6ffac34f9d6
Chris Stucchio <stucchio@gmail.com>
2011-04-17 13:49:39 -0400

    Added comments to thrift interface.

--- maryjane.thrift
+++ maryjane.thrift
@@ -14,10 +14,12 @@ exception MaryJaneFormatException {
   1: string error_msg
 }
 
-
-
 service MaryJane {
-  Timestamp sync(1:string streamname) throws (1:MaryJaneException mje, 2:MaryJaneStreamNotFoundException mje2),
   Timestamp addRecord(1:string streamname, 2:string key, 3:string value) throws (1:MaryJaneException mje, 2:MaryJaneStreamNotFoundException mje2, 3:MaryJaneFormatException mje3),
+  /* Adds a record to the specified stream. Key and value may not contain either tabs or newlines.
+   */
+  Timestamp sync(1:string streamname) throws (1:MaryJaneException mje, 2:MaryJaneStreamNotFoundException mje2),
+  /* Upload all available data from the stream to Hadoop.*/
   Timestamp syncAll() throws (1:MaryJaneException mje),
+   /* Upload all available data in all streams to Hadoop.*/
 }
