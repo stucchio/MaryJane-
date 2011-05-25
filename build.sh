50c332a08a4acfefca107b01ed4e9ba17a82f58d
Chris Stucchio <stucchio@gmail.com>
2011-05-25 06:47:41 +0530

    Now build script uses sed to strip slf4j from generated java source code.

--- build.sh
+++ build.sh
@@ -1,7 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
+function remove_slf4j(){
+    local filepath=$1;
+    local tmppath=`mktemp`;
+    sed '/slf4j/d' $filepath | sed '/LOGGER/d' > $tmppath
+    mv $tmppath $1
+}
 
 echo "Generating thrift interface for java."
 thrift --gen java maryjane.thrift
+echo "Stripping slf4j from generated thrift files."
+for fn in `ls gen-java/org/styloot/maryjane/gen/*.java`;
+do
+    remove_slf4j $fn;
+done;
+
 echo "Now building."
 ant compile
 ant maryjane
